2724e7f94c381f142b39a5efff999c7df6e0a4e3
Opilane TTHK <opilane@tthk.ee>
2021-09-01 12:26:02 +0300

    add readline

---
+++ FullName/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja käest kasutaja eesnime
+            //programm küsib kasutaja käest kasutaja perekonnanime
+            //programm tervitab kasutajat kasutades neid andmeid
+
+            Console.WriteLine("What is your first name?");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string LastName = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {FirstName} {LastName}");
+
+
+
+        }
+    }
+}
